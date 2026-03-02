45188a8e9e88d4c28276b3af843de575909b73c5
eloflo41 <elo.robert41@gmail.com>
2026-03-02 16:08:21 +0100

    modif

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // On force la base à '/' pour que Vercel trouve toujours le dossier /assets
+  base: '/', 
+  build: {
+    // On s'assure que le dossier de sortie est bien celui attendu par Vercel
+    outDir: 'dist',
+  }
 })
