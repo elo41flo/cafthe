17f29a068c268e9b9295c9b3818d08dd9eccf274
eloflo41 <elo.robert41@gmail.com>
2026-03-16 16:27:54 +0100

    compression image

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 import ProductCard from "../components/ProductCard";
-import "../styles/Pages/ProductList.css"; // Import du style
+import "../styles/Pages/ProductList.css";
 
 const ProductList = () => {
   const [produits, setProduits] = useState([]);
